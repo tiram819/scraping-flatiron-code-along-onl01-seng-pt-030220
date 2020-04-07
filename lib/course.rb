52f3e578425706574e17bc4d22cdb117bdbb24e9
Yeasha Hassan <tiram819@gmail.com>
2020-04-07 20:46:37 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,14 @@
 class Course
   attr_accessor :title, :schedule, :description
+  
+  @@all =[]
+  
+  def initialize
+    @@all << self 
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end
 end
 
